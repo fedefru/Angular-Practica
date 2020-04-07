bba325d282f4a96851773fd086e7a5c13f98098c
fedefru <fru.federico@gmail.com>
2020-04-07 13:42:29 -0300

    add proyect

---
+++ src/app/hero.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('Heroes obtenidos con exito!');
+    return of (HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: Heroe con id =${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
+  constructor(private messageService: MessageService) { }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Strange' },
+  { id: 12, name: 'Iron Man' },
+  { id: 13, name: 'Hulk' },
+  { id: 14, name: 'Capitan America' },
+  { id: 15, name: 'Spiderman' },
+  { id: 16, name: 'Thor' },
+  { id: 17, name: 'Black Widow' },
+  { id: 18, name: 'Star Lord' },
+  { id: 19, name: 'Ant-Man' },
+  { id: 20, name: 'Clint Barton' }
+];
